f0a83b71c19c1169d7e1af605bb4827fa0b6260a
hieuCM2895 <hieu2895@gmail.com>
2021-08-23 08:24:50 +0700

    try to hack system

---
+++ src/main/java/com/fpt/username/UserNameApplication.java
@@ -0,0 +1,14 @@
+package com.fpt.username;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserNameApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserNameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fpt/username/UserNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.username;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserNameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
